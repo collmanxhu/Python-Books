8221164981afecc352b0923051ee88311fb1bd8b
collmanxhu <collmanchu2010@gmail.com>
2020-01-12 19:14:32 +0800

    Signed-off-by: collmanxhu <collmanchu2010@gmail.com>

--- Python_for_data_analysis/git_conflict_test.py
+++ Python_for_data_analysis/git_conflict_test.py
@@ -2,3 +2,9 @@
 This Git tutorials provides a practical approach for understanding
 what git merge conflicts are and how to fix them.
 """
+
+def git_conflict_resolve():
+    a = 5
+    b = 6
+    c = 7
+    return a, b c
