48aeeed264e921c533234a25e072297891e61e6f
collmanxhu <collmanchu2010@gmail.com>
2020-01-12 21:48:07 +0800

    Update git_conflict_test.py

--- Python_for_data_analysis/git_conflict_test.py
+++ Python_for_data_analysis/git_conflict_test.py
@@ -23,3 +23,12 @@ def cars():
     brand = 'toyota'
     return brand
 
+alien_0 = {'color':'green', 'points':5}
+new_points = alien_0['points']
+print(f"You just earned {new_points} points!")
+print(alien_0)
+alien_0['x_position'] = 0
+alien_0['y_position'] = 25
+print(alien_0)
+alien_0['color'] = 'yellow'
+print(alien_0)
