128c5a0867861005c3a64d2eeff072fd7e9d6515
collmanxhu <collmanchu2010@gmail.com>
2020-01-27 20:29:19 +0800

    27-1-2017

--- Python_for_data_analysis/function_practice.py
+++ Python_for_data_analysis/function_practice.py
@@ -88,17 +88,37 @@ print(apply_to_list(ints, lambda x: x * 2))
 # list.sort(reverse=True|False, key=myFunc). Key=A function to specify the
 # sorting criteria(s)
 
-strings = ('foo', 'card', 'bar', 'aaaa', 'abab')
+strings = ['foo', 'card', 'bar', 'aaaa', 'abab']
 list_strings = list(strings)
-# print(strings)
+print(strings)
 print(list_strings)
-# print(list(strings))
-# print(set(list(strings)))
-# print(len(set(list(strings))))
-print(strings.sort(key=len(list_strings)))
+print(list(strings))
+print(set(list(strings)))
+print(len(set(list(strings))))
+strings.sort(key=lambda x: len(set(list(x))))
+print(strings)
 
-# strings.sort(key=lambda x: len(set(list(x))))
-# print(strings)
+data = [(4,5,6), (1,2,3), (7,8,9)]
+data.sort(key=lambda tup: tup[1])
+print(data) 
+
+""" 
+Currying: Partial Argument Application. means deriving new functions from
+existing ones by partial argument application.
+"""
+def add_number(x, y):
+    return x + y
+# using this functioin we could derive a new funciton of one variable, add_five
+# that adds  to its argument:
+add_five = lambda y: add_number(5, y)
+print(add_five(5))
+# There is nothing very fancy here. just define a new function to call an
+# existing function. The built-in functools module can simplify this process
+
+from functools import partial
+add_five = partial(add_number, 10)
+print(add_five(5))
+
 
 
 
